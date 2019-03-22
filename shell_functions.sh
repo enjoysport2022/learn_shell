f219f80f92f09105c0cac6422b1c4803ec64e1f0
caihengxing <caihengxing@didichuxing.com>
2019-03-22 18:13:06 +0800

    "Unix / Linux Shell Programming" finish

---
+++ shell_functions.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+Hello () {
+    echo "Hello world"
+}
+Hello
+
+#Pass Parameters to a Function
+Hello2 () {
+    echo "Hello world $1 $2"
+}
+Hello2 zara ali
+
+#Returning Values from Functions
+Hello3 () {
+    echo "Hello wrold $1"
+    return 10
+}
+Hello3 zara
+
+ret=$?
+echo "Return value is $ret"
+
+
+#Nested Functions
+number_one () {
+    echo "first func"
+    number_two
+}
+
+number_two () {
+    echo "second func"
+}
+
+number_one
+
+
+#Function Call from Prompt
+#You can put definitions for commonly used functions inside your .profile.
+# These definitions will be available whenever you log in and you can use them at the command prompt.
+
